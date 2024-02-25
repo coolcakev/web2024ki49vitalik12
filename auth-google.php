0c0c746015e03c2dd3fa3d71dbf4065111c52419
Vitalik Kravets <vitalikkravez1@gmail.com>
2024-02-25 22:26:15 +0200

    task4

---
+++ auth-google.php
@@ -0,0 +1,41 @@
+﻿<?php
+session_start();
+            $email = $_POST["email"];
+            $password= "temp";
+            $name="temp";
+
+         require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+           if ($user) {
+
+
+            }else{
+
+                $sql = "INSERT INTO users (email, password,name) VALUES ( ?, ?, ? )";
+            $stmt = mysqli_stmt_init($conn);
+            $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
+            if ($prepareStmt) {
+
+                mysqli_stmt_bind_param($stmt,"sss", $email, $password,$name);
+                mysqli_stmt_execute($stmt);
+
+                 $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+            }else{
+                die("Something went wrong");
+            }
+            }
+
+
+
+             $_SESSION["user"] = $user;
+                header("Location: index.php");
+                 die();
+
+
+?>
